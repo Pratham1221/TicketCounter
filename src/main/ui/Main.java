ab433cffd213efce7b5fadcd3965d5b2fd2b946d
pratham1221 <pratham.bansal21@gmail.com>
2024-01-12 19:07:20 -0800

    Testing git token

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,7 +6,7 @@ import model.Events;
 import java.util.Iterator;
 
 //This class runs the main program
-
+//Testing Git Token
 public class Main {
     public static void main(String[] args) throws Exception {
         new TicketCounter();
